4f785275978dfe2ec3f324647d67bbc838d1184c
Arai Gonzalez <araiestefania94@gmail.com>
2024-11-05 00:43:39 -0300

    completo

---
+++ javascript/contacto.js
@@ -0,0 +1,15 @@
+// Inicializar EmailJS
+(function(){
+    emailjs.init("m8cUYbeua1qgNHAAH");
+})();
+
+// Manejar el envío del formulario
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.sendForm('service_tuwx7cj', 'template_2716nge', this)
+        .then(function() {
+            alert('Correo enviado exitosamente!');
+        }, function(error) {
+            alert('Error al enviar el correo: ' + JSON.stringify(error));
+        });
+});
