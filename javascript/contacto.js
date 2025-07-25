81f9d1ae4db343c2b773de1134ba03906bace492
Arai Gonzalez <araigonzalez94@gmail.com>
2025-07-25 19:01:24 -0300

    chore: actualiza información personal

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -1,15 +1,19 @@
 // Inicializar EmailJS
-(function(){
-    emailjs.init("m8cUYbeua1qgNHAAH");
+(function () {
+  emailjs.init("m8cUYbeua1qgNHAAH");
 })();
 
 // Manejar el envío del formulario
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    emailjs.sendForm('service_tuwx7cj', 'template_2716nge', this)
-        .then(function() {
-            alert('Correo enviado exitosamente!');
-        }, function(error) {
-            alert('Error al enviar el correo: ' + JSON.stringify(error));
-        });
-});
+    emailjs.sendForm("service_tuwx7cj", "template_2716nge", this).then(
+      function () {
+        alert("Correo enviado exitosamente!");
+      },
+      function (error) {
+        alert("Error al enviar el correo: " + JSON.stringify(error));
+      }
+    );
+  });
